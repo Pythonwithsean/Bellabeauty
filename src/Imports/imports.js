55589cc8812e89fbce3513e302ec91067f5231a9
Pythonwithsean <Lunafcs@Protonmail.com>
2023-12-24 22:14:54 +0100

    "Commit"

--- src/Imports/imports.js
+++ src/Imports/imports.js
@@ -1,6 +1,6 @@
 import Signup from "../pages/auth/Signup";
 import Home from "../pages/Home";
-import Hero from "../components/Hero.jsx";
+
 import Location from "../components/Location.jsx";
 
-export { Signup, Home, Hero, Location };
+export { Signup, Home, Location };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import "../styles/home_styles.css"
-import Navbar from "../components/Navbar"
-import { Hero} from "../Imports/imports"
-import {Location} from "../Imports/imports"
+import React from "react";
+import "../styles/home_styles.css";
+import Navbar from "../components/Navbar";
+import { Location } from "../Imports/imports";
 
 function Home() {
   return (
     <>
-    <Location/>
-    <Navbar/>
-    <Hero/>
+      <Location />
+      <Navbar />
+      <div className="Hero">
+        <img
+          className="Hero_Img"
+          src="../../public/Untitled design (1).png"
+          alt=""
+        />
+
+        <div className="Item-2">
+          <h1>Bella beauty</h1>
+          <h2>Be Bold Be Beautiful</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+            voluptates, voluptatem, quibusdam, quas voluptatum voluptate
+            consequatur quod voluptatibus quae natus dolorum! Quisquam
+            voluptates, voluptatem, quibusdam, quas voluptatum voluptate
+            consequatur quod voluptatibus quae natus dolorum! Quisquam
+          </p>
+          <button type="button" className="Button">
+            Shop Now
+          </button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
