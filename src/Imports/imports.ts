996a5cba8cb890afca48d219bcfff5fa7c5a3a94
Pythonwithsean <Lunafcs@Protonmail.com>
2024-01-28 10:59:05 +0000

    "Commit"

--- src/Imports/imports.ts
+++ src/Imports/imports.ts
@@ -3,6 +3,6 @@ import Home from "../pages/Home";
 import NotFound from "../pages/NotFound.js";
 
 import Location from "../components/Location.js";
-import { AuthProvider, auth, type AuthcontextType } from "../pages/auth/authProvider.js";
+import { AuthProvider, auth, type AuthcontextType } from "../pages/auth/AuthProvider.js";
 
 export { Signup, Home, Location, NotFound, AuthProvider, auth, type AuthcontextType };
